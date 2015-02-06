61582ae34c12f9b2bcc8814a9ddff2522c0b6f31
arunma <arun@arunma.com>
2015-02-06 09:32:22 +0800

    working set - headers and data push fixed.

--- src/main/java/com/arunma/dungeon/converters/HeaderProcessor.java
+++ src/main/java/com/arunma/dungeon/converters/HeaderProcessor.java
@@ -3,8 +3,8 @@ package com.arunma.dungeon.converters;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,14 +13,21 @@ import java.util.Map;
 public class HeaderProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
-        HashSet<String> set= (HashSet<String>) exchange.getIn().getHeader("CamelJdbcColumnNames");
+        //HashSet<String> set= (HashSet<String>) exchange.getIn().getHeader("CamelJdbcColumnNames");
 
-        Map<String, Object> map = new HashMap<String, Object>();
-        for (String string : set) {
-            map.put(string, string);
+        List<LinkedHashMap<String, Object>> allData = (List<LinkedHashMap<String, Object>>)exchange.getIn().getBody();
+
+        if (allData.size()==0) return;
+
+        LinkedHashMap<String,String> headerMap=new LinkedHashMap<String, String>();
+
+        for (Map.Entry<String, Object> eachEntry : allData.get(0).entrySet()) {
+            String headerKey=eachEntry.getKey();
+            headerMap.put(headerKey,headerKey);
+            System.out.println(headerKey);
         }
 
-        exchange.getOut().setBody(map);
+        exchange.getOut().setBody(headerMap);
 
     }
 }
